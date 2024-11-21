3ccbae25fcdbfd8b2ff1956c2a9c09ee131c872d
huy.lyquang <huy.lyquang@ncc.asia>
2024-11-21 19:02:36 +0700

    'remove obj'

---
+++ Object Repository/Channel Message/Send emoji, sticker, GIF/Page_Mezon/Button_Emojis pannel.rs
@@ -0,0 +1,110 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Button_Emojis pannel</name>
+   <tag></tag>
+   <elementGuidId>2098e857-91cf-4566-8c78-4e6de3e28ebe</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>(.//*[normalize-space(text()) and normalize-space(.)='.'])[16]/following::*[name()='svg'][4]</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>svg</value>
+      <webElementGuid>73b17419-5e26-42e5-9eae-eea7e330df91</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>dark:hover:text-white hover:text-black w-6 h-6 dark:text-[#B5BAC1] text-colorTextLightMode</value>
+      <webElementGuid>e43ad9e4-2be3-4a78-9a1d-b85fbfe984e0</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>aria-hidden</name>
+      <type>Main</type>
+      <value>true</value>
+      <webElementGuid>0d419c6c-e123-43c8-8421-df11c4c82db1</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>role</name>
+      <type>Main</type>
+      <value>img</value>
+      <webElementGuid>84cc9005-ae26-4f51-be07-81f378d97d1d</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xmlns</name>
+      <type>Main</type>
+      <value>http://www.w3.org/2000/svg</value>
+      <webElementGuid>2a01dfc7-70a5-46f3-8aa1-867d0a780059</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>width</name>
+      <type>Main</type>
+      <value>24</value>
+      <webElementGuid>11afcc10-b7c4-4a41-b007-129ff248b03c</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>height</name>
+      <type>Main</type>
+      <value>24</value>
+      <webElementGuid>3cd0a402-600e-4c14-8e7f-2c397043bc32</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>viewBox</name>
+      <type>Main</type>
+      <value>0 0 24 24</value>
+      <webElementGuid>8c254cd5-3982-44cf-8388-0b0ad49d09fc</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;mainChat&quot;)/div[@class=&quot;flex flex-row h-heightWithoutTopBar&quot;]/div[@class=&quot;flex flex-col flex-1 min-w-60 w-widthThumnailAttachment h-full false z-10&quot;]/div[@class=&quot;flex-shrink flex flex-col dark:bg-bgPrimary bg-bgLightPrimary h-auto relative w-full&quot;]/div[@class=&quot;mx-4 relative&quot;]/div[@class=&quot;relative max-sm:-pb-2&quot;]/div[@class=&quot;flex flex-inline items-start gap-2 box-content mb-4 max-sm:mb-0
+				 dark:bg-channelTextarea bg-channelTextareaLight rounded-lg relative rounded-t-lg
+				  w-wrappBoxChatView&quot;]/div[@class=&quot;w-full dark:bg-channelTextarea bg-channelTextareaLight gap-3 flex items-center rounded-e-md&quot;]/div[@class=&quot;w-full rounded-r-lg dark:bg-channelTextarea bg-channelTextareaLight gap-3 relative whitespace-pre-wrap&quot;]/div[@class=&quot;relative&quot;]/div[@class=&quot;flex flex-row absolute h-11 items-center gap-1 mr-3 top-0 right-0&quot;]/div[@class=&quot;w-6 h-6&quot;]/svg[@class=&quot;dark:hover:text-white hover:text-black w-6 h-6 dark:text-[#B5BAC1] text-colorTextLightMode&quot;]</value>
+      <webElementGuid>5c193724-3a8d-449a-9cfe-658fc20d1413</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='.'])[16]/following::*[name()='svg'][4]</value>
+      <webElementGuid>fef149f9-dc14-4f0c-aac9-ef8c10a0d0cc</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='.'])[15]/following::*[name()='svg'][4]</value>
+      <webElementGuid>2fe583d1-3f1e-4748-a25f-669fbd3c2952</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
