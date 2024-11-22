8601e49c6578e580ba34e1af5fe5084a3cc2da71
tien-cao <tien.caothicam@ncc.asia>
2024-11-22 06:56:24 +0000

    feature copy message in channel message

---
+++ Object Repository/Channel Message/Edit, Reply, Forward, Copy, Delete Message/div_Copy Text.rs
@@ -0,0 +1,106 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_Copy Text</name>
+   <tag></tag>
+   <elementGuidId>19b24389-747c-4231-a1ff-94e80fa3c406</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='mainChat']/div/div/div/div[2]/div[9]/div/div</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorCollection>
+      <entry>
+         <key>SMART_LOCATOR</key>
+         <value>internal:role=menuitem[name=&quot;Copy Text&quot;i] >> div >> nth=1</value>
+      </entry>
+   </smartLocatorCollection>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+      <webElementGuid>44bbbff4-23ac-47cb-8d77-3bb4fed3dd07</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value> dark:text-[#ADB3B9] text-[#4E5058] hover:text-[#FFFFFF] dark:hover:text-[#FFFFFF]  p-1</value>
+      <webElementGuid>f46cbbb4-6db1-47e8-923b-3ded361bf288</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Copy Text </value>
+      <webElementGuid>aa95358c-7982-48d9-8d6c-658c8a566297</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;mainChat&quot;)/div[@class=&quot;flex flex-row h-heightWithoutTopBar&quot;]/div[@class=&quot;flex flex-col flex-1 min-w-60 w-widthThumnailAttachment h-full false z-10&quot;]/div[@class=&quot;relative dark:bg-bgPrimary max-w-widthMessageViewChat bg-bgLightPrimary h-heightMessageViewChat&quot;]/div[@class=&quot;contexify z-50 contexify_willEnter-fade&quot;]/div[@class=&quot;contexify_item&quot;]/div[@class=&quot;contexify_itemContent&quot;]/div[@class=&quot;dark:text-[#ADB3B9] text-[#4E5058] hover:text-[#FFFFFF] dark:hover:text-[#FFFFFF]  p-1&quot;]</value>
+      <webElementGuid>ee07c2f6-19ed-4860-b18f-2a8bfb5e14c6</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='mainChat']/div/div/div/div[2]/div[9]/div/div</value>
+      <webElementGuid>37e85d88-f670-4acf-badc-ad9f7e5a9956</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Create Thread'])[1]/following::div[3]</value>
+      <webElementGuid>2b3c406b-85e5-4ffd-a182-a45c293d85e4</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Reply'])[1]/following::div[6]</value>
+      <webElementGuid>1c08c383-7c9e-40fa-9d7f-e8956900694e</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Apps'])[1]/preceding::div[1]</value>
+      <webElementGuid>ee593f3b-8458-4de1-afc9-41135cdea361</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[2]/div[9]/div/div</value>
+      <webElementGuid>1ac98017-29ae-4f31-97f5-1ac204a6ee47</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//div[(text() = 'Copy Text ' or . = 'Copy Text ')]</value>
+      <webElementGuid>4a022cbb-05c5-4438-a5e9-25d730dbc42a</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
