c6e1d84aed50a03ec88b05f0ef39f550fd6f1759
NguyenNCC <nguyen.nguyenphuoc@ncc.asia>
2024-11-24 16:25:49 +0700

    update: TC for verify msg after sent

--- Scripts/Steps/Channel Message/Send Message Text/Script1731576204107.groovy
+++ Scripts/Steps/Channel Message/Send Message Text/Script1731576204107.groovy
@@ -26,7 +26,10 @@ WebUI.setText(findTestObject('Page_Mezon/textarea_input_message_channel'), 'Hi N
 
 WebUI.sendKeys(findTestObject('Object Repository/Page_Mezon/textarea_input_message_channel'), Keys.chord(Keys.ENTER))
 
-WebUI.verifyElementVisible(findTestObject('Channel Message/Select channel and send message/span_text_message_channel'))
+WebUI.waitForElementVisible(findTestObject('Channel Message/Edit, Reply, Forward, Copy, Delete Message/msg_after_send'), 
+    0)
+
+WebUI.verifyElementVisible(findTestObject('Channel Message/Edit, Reply, Forward, Copy, Delete Message/msg_after_send'))
 
 WebUI.closeBrowser()
 
