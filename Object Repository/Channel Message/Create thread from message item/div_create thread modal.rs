b214ebfe932c7b6e68a74cbd8d55402c2f63fa95
huy.lyquang <huy.lyquang@ncc.asia>
2024-11-29 11:23:50 +0700

    test case: create thread from message item

---
+++ Object Repository/Channel Message/Create thread from message item/div_create thread modal.rs
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_create thread modal</name>
+   <tag></tag>
+   <elementGuidId>a89d861d-f30f-4b9f-83ef-a24bfaace030</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//*[@id=&quot;main-layout&quot;]/div/div[4]</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Object Repository/Channel Message/Create thread from message item/texarea_send message.rs
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>texarea_send message</name>
+   <tag></tag>
+   <elementGuidId>2606cc34-5f42-40f3-b1ab-cdc52ba47a87</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>/html/body/div[2]/div/div[2]/div/div[4]/div/div[2]/div[2]/div/div[2]/div/textarea</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
