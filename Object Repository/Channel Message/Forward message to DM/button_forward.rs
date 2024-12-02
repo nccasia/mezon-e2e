9c0ea92d3242bc1d67315ce771ea4dda86ada94f
huy.lyquang <huy.lyquang@ncc.asia>
2024-12-02 13:50:59 +0700

    test case: Forward message to DM

---
+++ Object Repository/Channel Message/Forward message to DM/button_forward.rs
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>button_forward</name>
+   <tag></tag>
+   <elementGuidId>1d95f4c5-7509-4fc6-afc7-5f4852d00286</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//*[@id=&quot;mainChat&quot;]/div/div/div[1]/div[2]/div[13]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
