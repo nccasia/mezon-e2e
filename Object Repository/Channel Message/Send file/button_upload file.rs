0ec639b7f122c2e9dcce53780d3535d4f677dc60
huy.lyquang <huy.lyquang@ncc.asia>
2024-12-04 13:23:09 +0700

    test case: send file

---
+++ Object Repository/Channel Message/Send file/button_upload file.rs
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>button_upload file</name>
+   <tag></tag>
+   <elementGuidId>60ad5706-22ea-4458-957d-a9af58907832</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>/html/body/div[2]/div[2]/div[2]/div/div[3]/div/div[2]/div/div/div/div/div[2]/div[1]/div[2]/div/label/div</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
