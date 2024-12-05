e986831b0e28582fea2336e280c884473dc0e213
HangPhuong <phuong.nguyenhonghang@ncc.asia>
2024-12-05 09:25:53 +0700

    fix TCs

--- Scripts/Steps/Channel Message/Edit Message/Script1732508961873.groovy
+++ Scripts/Steps/Channel Message/Edit Message/Script1732508961873.groovy
@@ -16,6 +16,9 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
+import org.openqa.selenium.WebElement as WebElement
+import com.kms.katalon.core.testobject.ConditionType as ConditionType
+import com.kms.katalon.core.util.KeywordUtil as KeywordUtil
 
 WebUI.callTestCase(findTestCase('Steps/Login_Logout and SignUp/Login with email and password'), [('email') : 'E2E1762357@ncc.asia'
         , ('password') : 'E2E1762357'], FailureHandling.STOP_ON_FAILURE)
@@ -27,15 +30,39 @@ WebUI.click(findTestObject('Object Repository/Channel Message/Edit, Reply, Forwa
 CustomKeywords.'mezon.SendText.sendText'(findTestObject('Channel Message/Edit, Reply, Forward, Copy, Delete Message/textarea_clanT_general'), 
     'chi Phuong xinh dep', Keys.chord(Keys.ENTER))
 
-WebUI.mouseOver(findTestObject('Channel Message/Edit, Reply, Forward, Copy, Delete Message/div_latest message'))
+TestObject divLastedObj = findTestObject('Channel Message/Edit, Reply, Forward, Copy, Delete Message/div_lasted')
+
+WebElement divLastedElm = WebUI.findWebElement(divLastedObj)
+
+String idMessage = divLastedElm.getAttribute('id')
+
+String spanLasted = "//*[@id='$idMessage']/div[1]/div[2]/div/div[2]/div[2]/div/div/div/div/div/span/span"
+
+WebUI.mouseOver(divLastedObj)
 
 WebUI.click(findTestObject('Channel Message/Edit, Reply, Forward, Copy, Delete Message/edit_btn'))
 
-WebUI.sendKeys(findTestObject('Channel Message/Edit, Reply, Forward, Copy, Delete Message/textarea_need_edit'), Keys.chord(Keys.CONTROL, 'a'))
-WebUI.sendKeys(findTestObject('Channel Message/Edit, Reply, Forward, Copy, Delete Message/textarea_need_edit'), "hehe")
+WebUI.sendKeys(findTestObject('Channel Message/Edit, Reply, Forward, Copy, Delete Message/textarea_need_edit'), Keys.chord(
+        Keys.CONTROL, 'a'))
 
+String editMessage = 'hehe'
 
-WebUI.verifyTextPresent('hehe', true)
+WebUI.sendKeys(findTestObject('Channel Message/Edit, Reply, Forward, Copy, Delete Message/textarea_need_edit'), editMessage)
 
+WebUI.sendKeys(findTestObject('Channel Message/Edit, Reply, Forward, Copy, Delete Message/textarea_need_edit'), Keys.chord(
+        Keys.ENTER))
 
+TestObject spanLastedObj = getTestObjectWithXpath(spanLasted)
+
+WebElement spanLastedElm = WebUI.findWebElement(spanLastedObj)
+
+if (editMessage == spanLastedElm.getText()) {
+	KeywordUtil.markPassed("pass")
+} else {
+    KeywordUtil.markFailed("fail")
+}
+
+static TestObject getTestObjectWithXpath(String xpath) {
+    return new TestObject().addProperty('xpath', ConditionType.EQUALS, xpath)
+}
 
