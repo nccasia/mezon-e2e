6b57f697eab088b7f1e3c927d207ad38cb38adac
huy.lyquang <huy.lyquang@ncc.asia>
2024-12-23 13:43:41 +0700

    finished test case load more message

---
+++ Object Repository/Channel Message/Load more message/div_message container.rs
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_message container</name>
+   <tag></tag>
+   <elementGuidId>02db24f8-5497-4f1c-9786-6311222050e1</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//*[@id=&quot;scrollLoading&quot;]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
