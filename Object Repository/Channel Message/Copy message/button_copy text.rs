5a447fda4c010944a3498d9b938c14b943cd8c53
huy.lyquang <huy.lyquang@ncc.asia>
2024-12-27 10:04:58 +0700

    test case copy message

---
+++ Object Repository/Channel Message/Copy message/button_copy text.rs
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>button_copy text</name>
+   <tag></tag>
+   <elementGuidId>955e4885-8895-469a-9f70-60a20e3f1639</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//*[@id=&quot;mainChat&quot;]/div/div/div[1]/div[2]/div[contains(@class, 'contexify_item')][7]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
