d99613ebfc4797c893214ba42bf275d3ff199cc9
huy.lyquang <huy.lyquang@ncc.asia>
2024-12-31 13:24:12 +0700

    init test case create and send msg to topic

---
+++ Object Repository/Channel Message/Create and send msg to topic/div_msg container.rs
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_msg container</name>
+   <tag></tag>
+   <elementGuidId>2f9c445a-df2f-41ca-903c-a0c5d85245c7</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>/html/body/div[2]/div[2]/div[2]/div/div[4]/div/div[2]/div/div[3]/div/div[@data-index][last()]/div/div/div[contains(@class,'message-container')]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Object Repository/Channel Message/Create and send msg to topic/span_latest message.rs
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>span_latest message</name>
+   <tag></tag>
+   <elementGuidId>c50fe1f6-cb25-42ec-95ff-f3b45b6a86e8</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>/html/body/div[2]/div[2]/div[2]/div/div[4]/div/div[2]/div/div[3]/div/div[@data-index][last()]/div/div/div/div[1]/div[2]/div/div[2]/div[2]/div/div/div/div/div/div/span</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
