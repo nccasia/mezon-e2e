62ab94a6fb8991c8394b03ff61d42b3ac0c709a3
huy.lyquang <huy.lyquang@ncc.asia>
2025-01-12 21:32:43 +0700

    add test case: add a new sticker in clan

---
+++ Object Repository/Manage Clan/Add a New Sticker in Clan/input_Upload Sticker.rs
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>input_Upload Sticker</name>
+   <tag></tag>
+   <elementGuidId>b89e55c4-116b-4384-8014-18576a82b6ce</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//*[@id=&quot;main-layout&quot;]/div/div[2]/div[2]/div/div[4]/div/div[1]/div/div[2]/div/div/div[2]/div/div/div[1]/div[3]/div[1]/div/button/input</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Object Repository/Manage Clan/Add a New Sticker in Clan/p_New Sticker.rs
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>p_New Sticker</name>
+   <tag></tag>
+   <elementGuidId>e9aa2694-c07b-4263-8526-7d2c4b14b147</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//*[@id=&quot;main-layout&quot;]/div/div[2]/div[2]/div/div[4]/div/div[1]/div/div/div[3]/div[1]/p</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
