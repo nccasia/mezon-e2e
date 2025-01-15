04a8daacafec9842ba87443dd291471c68b950fa
huy.lyquang <huy.lyquang@ncc.asia>
2025-01-15 16:02:39 +0700

    add test case: create and delete canvas

---
+++ Object Repository/Manage Channel/Create and Delete Canvas/breadcrumb_Canvas Title.rs
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>breadcrumb_Canvas Title</name>
+   <tag></tag>
+   <elementGuidId>6273e34a-19ba-4551-822a-38208e9eccba</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//*[@id=&quot;main-layout&quot;]/div/div[3]/div/div[1]/div[1]/div/div[2]/div/p</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Object Repository/Manage Channel/Create and Delete Canvas/div_canvas container.rs
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_canvas container</name>
+   <tag></tag>
+   <elementGuidId>cfbfb9bc-e4b2-4f0d-a374-02a1bc33463e</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//*[@id=&quot;main-layout&quot;]/div/div[3]/div/div[1]/div[2]/div/div[1]/div[1]/div[2]/div/div/div[2]/div[last()]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
