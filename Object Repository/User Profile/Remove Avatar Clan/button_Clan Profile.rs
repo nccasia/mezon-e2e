880dd11a43f009c9fa32547a00d235060cad1eaa
huy.lyquang <huy.lyquang@ncc.asia>
2025-01-20 11:26:35 +0700

    add test case: remove avatar clan

---
+++ Object Repository/User Profile/Remove Avatar Clan/button_Clan Profile.rs
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>button_Clan Profile</name>
+   <tag></tag>
+   <elementGuidId>a15bb2ac-fab9-4a39-a2d3-2af35750d26e</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//*[@id=&quot;main-layout&quot;]/div/div[4]/div/div/div[2]/div[1]/div/button[2]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
