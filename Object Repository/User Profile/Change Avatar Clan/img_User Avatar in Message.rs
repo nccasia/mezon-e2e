9c587b8844f702a6c1f7b34359b65781df4dc17a
huy.lyquang <huy.lyquang@ncc.asia>
2025-01-20 09:30:48 +0700

    add test case: change avatar clan

---
+++ Object Repository/User Profile/Change Avatar Clan/img_User Avatar in Message.rs
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>img_User Avatar in Message</name>
+   <tag></tag>
+   <elementGuidId>e51a5dc0-ef6b-4a00-b027-c7e664f3553b</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//*[@id='scrollLoading']/div/div[@class='message-list-item'][last()]/div/div/div/div[1]/div[2]/div/div[1]/img</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
