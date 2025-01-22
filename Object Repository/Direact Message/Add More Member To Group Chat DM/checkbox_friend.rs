84473f6d7b31a045c64fab6c3c3439afb65dbbac
huy.lyquang <huy.lyquang@ncc.asia>
2025-01-22 14:30:52 +0700

    add test case: add more member to group chat DM

---
+++ Object Repository/Direact Message/Add More Member To Group Chat DM/checkbox_friend.rs
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>checkbox_friend</name>
+   <tag></tag>
+   <elementGuidId>d17768d8-5004-4fe1-958c-ec74df74e603</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//*[@id=&quot;checkbox-item-0&quot;]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
