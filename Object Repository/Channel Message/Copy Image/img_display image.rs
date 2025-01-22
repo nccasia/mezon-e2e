775dc3557c451d82962932dd82aff6fcba6d7aef
huy.lyquang <huy.lyquang@ncc.asia>
2025-01-22 11:10:28 +0700

    add test case: copy image

---
+++ Object Repository/Channel Message/Copy Image/img_display image.rs
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>img_display image</name>
+   <tag></tag>
+   <elementGuidId>ac2f05cc-c06a-46a9-82b4-066dedf41dc6</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//*[@id=&quot;main-layout&quot;]/div/div[3]/div[2]/div[1]/div[2]/div[1]/div[2]/div[1]/div/div/div/div[1]/div/img</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
